ca6cb95b3f160a06f0c7ca9ba51143f96c9b3aa4
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2025-12-19 20:38:35 +0530

    Create Break.py

---
+++ Break.py
@@ -0,0 +1,8 @@
+# Program to demonstrate break and continue statements
+
+print("Demonstrating 'break' statement:")
+for i in range(1, 11):
+    if i == 5:
+        print("Break encountered at i =", i)
+        break
+    print(i)
