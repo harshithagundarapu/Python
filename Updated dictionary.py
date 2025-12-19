44a1cc7f52ebe52bcce506f5eb55d120bbc276af
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2025-12-19 20:21:03 +0530

    Create Updated dictionary.py

---
+++ Updated dictionary.py
@@ -0,0 +1,10 @@
+# Python program to create a dictionary and update it with a new key-value pair
+# Creating a dictionary
+student = {
+    "name": "Harshi",
+    "age": 20,
+    "course": "B.Tech"
+}
+# Adding a new key-value pair
+student["year"] = 2025
+print(student)
