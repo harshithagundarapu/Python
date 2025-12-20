11ba895aa1b02c839077a8e84273fdd23c86f00a
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2025-12-20 20:26:15 +0530

    Create Key value pairs.py

---
+++ Key value pairs.py
@@ -0,0 +1,4 @@
+# Create a dictionary where key = number (1–10) and value = 2 × key
+numbers = {key: key * 2 for key in range(1, 11)}
+print("Key-Value Pairs (1–10 and their doubles):")
+print(numbers)
