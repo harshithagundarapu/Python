6cfa0a09e54713bf9b8d46ae7d3f4e0fdd2ff257
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2025-12-21 13:41:17 +0530

    Create Bitwise operators.py

---
+++ Bitwise operators.py
@@ -0,0 +1,8 @@
+a = 10 # 1010 in binary
+b = 4 # 0100 in binary
+print("a & b:", a & b) 
+print("a | b:", a | b) 
+print("a ^ b:", a ^ b) 
+print("~a:", ~a) 
+print("a << 2:", a << 2) 
+print("a >> 2:", a >> 2) 
