841d8cbba235867d0f60755a0543e99c97673532
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2025-12-31 20:47:28 +0530

    Create Random joke generator.py

---
+++ Random joke generator.py
@@ -0,0 +1,8 @@
+import random
+
+jokes = [
+    "Why do programmers love Python? Because it has no braces!",
+    "Why was the computer cold? It forgot to close windows."
+]
+
+print(random.choice(jokes))
