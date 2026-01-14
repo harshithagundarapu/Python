cf6a7b33f025a9f61aa3cb2eedbb985d8c198abd
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-01-14 20:43:38 +0530

    Create Check palindrome number.py

---
+++ Check palindrome number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter number: "))
+temp = num
+rev = 0
+
+while num > 0:
+    rev = rev * 10 + num % 10
+    num //= 10
+
+if temp == rev:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome")
