787423450062b54dfcafcdf772115583b4017bde
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-01-16 10:59:55 +0530

    Create Check  perfect number.py

---
+++ Check  perfect number.py
@@ -0,0 +1,11 @@
+num = int(input("Enter number: "))
+s = 0
+
+for i in range(1, num):
+    if num % i == 0:
+        s += i
+
+if s == num:
+    print("Perfect Number")
+else:
+    print("Not a Perfect Number")
