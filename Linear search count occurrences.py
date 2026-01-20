4fe06860b217060852428d0b173c28c1ef604bc2
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-01-20 10:55:01 +0530

    Create Linear search count occurrences.py

---
+++ Linear search count occurrences.py
@@ -0,0 +1,9 @@
+arr = list(map(int, input("Enter elements: ").split()))
+key = int(input("Enter element: "))
+count = 0
+
+for i in arr:
+    if i == key:
+        count += 1
+
+print("Occurrences:", count)
