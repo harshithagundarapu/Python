c287b77a91613e1b29ccfb62de61fe0750414a04
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-01-22 20:08:57 +0530

    Create Quick sort.py

---
+++ Quick sort.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x <= pivot]
+    right = [x for x in arr[1:] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+arr = list(map(int, input("Enter elements: ").split()))
+print("Sorted list:", quick_sort(arr))
