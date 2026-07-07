f9886f718d345166e965dd073f7a0f25158df62c
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-07 20:16:53 +0530

    Add J pattern printing functionality
    Implement a pattern printing program that outputs a 'J' shape using asterisks.

---
+++ J pattern.py
@@ -0,0 +1,16 @@
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if i == 0:
+            print("*", end="")
+        elif j == cols // 2 and i < rows - 1:
+            print("*", end="")
+        elif i == rows - 1 and j < cols // 2:
+            print("*", end="")
+        elif j == 0 and i >= rows - 2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
