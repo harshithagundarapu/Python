f095720f3ff65b752cf7df822ee15e3bc267e778
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-07 20:14:02 +0530

    Create G pattern.py

---
+++ G pattern.py
@@ -0,0 +1,16 @@
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if (i == 0 and j > 0) or (i == rows - 1 and j > 0):
+            print("*", end="")
+        elif (j == 0 and i > 0 and i < rows - 1):
+            print("*", end="")
+        elif (i >= rows // 2 and j == cols - 1):
+            print("*", end="")
+        elif (i == rows // 2 and j >= cols // 2):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
