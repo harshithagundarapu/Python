aa57eedd8981e5cc7bebb8b2f6ccc9a9442e1b3e
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-08 20:02:12 +0530

    Add K pattern printing program

---
+++ K pattern.py
@@ -0,0 +1,10 @@
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if j == 0 or i + j == rows // 2 or i - j == rows // 2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
