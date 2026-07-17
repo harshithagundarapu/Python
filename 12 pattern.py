90f4921bfe650c2198c418b5eee9337054f0e2b8
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-17 22:29:07 +0530

    Create 12 pattern.py

---
+++ 12 pattern.py
@@ -0,0 +1,24 @@
+rows = 7
+
+for i in range(rows):
+
+    # Digit 1
+    for j in range(5):
+        if j == 2 or (i == 1 and j == 1) or i == rows - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")  # Space between digits
+
+    # Digit 2
+    for j in range(5):
+        if i == 0 or i == rows // 2 or i == rows - 1:
+            print("*", end="")
+        elif i < rows // 2 and j == 4:
+            print("*", end="")
+        elif i > rows // 2 and j == 0:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
