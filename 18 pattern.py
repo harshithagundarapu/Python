f5f2a0ac285cd1d841367cd8ad472e2b96bec4a2
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-20 21:39:24 +0530

    Implement digit pattern printing in 18 pattern.py
    Add a pattern printing program for digits 1 and 8.

---
+++ 18 pattern.py
@@ -0,0 +1,21 @@
+rows = 7
+
+for i in range(rows):
+
+    # Digit 1
+    for j in range(5):
+        if j == 2 or (i == 1 and j == 1) or i == rows - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")  # Space between digits
+
+    # Digit 8
+    for j in range(5):
+        if ((i == 0 or i == rows // 2 or i == rows - 1) and (j > 0 and j < 4)) or \
+           ((j == 0 or j == 4) and (i > 0 and i < rows - 1)):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
