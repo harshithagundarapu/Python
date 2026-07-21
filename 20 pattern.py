981006adf11ee9c15fc742333d5f33d1bbfd9832
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-21 20:55:36 +0530

    Create pattern.py to display digits 2 and 0
    Added a Python script to print the digits '2' and '0' using asterisks.

---
+++ 20 pattern.py
@@ -0,0 +1,25 @@
+rows = 7
+
+for i in range(rows):
+
+    # Digit 2
+    for j in range(5):
+        if i == 0 or i == rows // 2 or i == rows - 1:
+            print("*", end="")
+        elif i < rows // 2 and j == 4:
+            print("*", end="")
+        elif i > rows // 2 and j == 0:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")  # Space between digits
+
+    # Digit 0
+    for j in range(5):
+        if ((i == 0 or i == rows - 1) and (j > 0 and j < 4)) or \
+           ((j == 0 or j == 4) and (i > 0 and i < rows - 1)):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
