8d84cd29bb49ce9e6c9b8a30b3ce2d22f42a2140
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-07-29 21:05:50 +0530

    Add script to print pattern of digits 3 and 6
    This script prints a pattern of digits 3 and 6 using asterisks.

---
+++ 36 pattern.py
@@ -0,0 +1,21 @@
+rows = 7
+
+for i in range(rows):
+
+    # Digit 3
+    for j in range(5):
+        if i == 0 or i == rows // 2 or i == rows - 1:
+            print("*", end="")
+        elif j == 4:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")  # Space between digits
+
+    # Digit 6
+    for j in range(5):
+        if (i == 0 and j > 0) or \
+           (i == rows // 2) or \
+           (i == rows - 1 and j > 0) or \
+           (j == 0 and i > 0 and i < rows - 1)
