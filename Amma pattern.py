535d393c7b76e8e34f98bef0a44d67d9b86c9ea8
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-08-01 10:14:55 +0530

    Add pattern printing for 'AMMA' in Python

---
+++ Amma pattern.py
@@ -0,0 +1,46 @@
+rows = 7
+
+for i in range(rows):
+
+    # A
+    for j in range(7):
+        if ((j == 0 or j == 6) and i != 0) or \
+           (i == 0 and j == 3) or \
+           (i == 3 and j > 0 and j < 6):
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")
+
+    # M
+    for j in range(7):
+        if j == 0 or j == 6 or \
+           (i == j and i <= 3) or \
+           (i + j == 6 and i <= 3):
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")
+
+    # M
+    for j in range(7):
+        if j == 0 or j == 6 or \
+           (i == j and i <= 3) or \
+           (i + j == 6 and i <= 3):
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print("  ", end="")
+
+    # A
+    for j in range(7):
+        if ((j == 0 or j == 6) and i != 0) or \
+           (i == 0 and j == 3) or \
+           (i == 3 and j > 0 and j < 6):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
