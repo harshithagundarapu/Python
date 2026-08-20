0defcbff15676c64b3ee5abeb1bfc36a88afc5a0
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-08-20 21:36:42 +0530

    Add pattern printing for 'TEN' in Ten.py

---
+++ Ten.py
@@ -0,0 +1,26 @@
+for i in range(5):
+
+    # T
+    for j in range(5):
+        if i == 0 or j == 2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("   ", end="")
+
+    # E
+    for j in range(5):
+        if j == 0 or i == 0 or i == 2 or i == 4:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("   ", end="")
+
+    # N
+    for j in range(5):
+        if j == 0 or j == 4 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
