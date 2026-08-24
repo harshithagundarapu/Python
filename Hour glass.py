42817072fb6baf81682e578d263e0656996ba5c3
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-08-24 22:15:11 +0530

    Add hourglass pattern printing functionality
    Implement hourglass pattern printing in Python.

---
+++ Hour glass.py
@@ -0,0 +1,17 @@
+for i in range(5, 0, -1):
+    for j in range(5 - i):
+        print(" ", end=" ")
+
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+
+    print()
+
+for i in range(2, 6):
+    for j in range(5 - i):
+        print(" ", end=" ")
+
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+
+    print()
