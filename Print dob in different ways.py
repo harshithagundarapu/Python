6d98d921cef00e0f468fab689a567160fc6b836f
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-09-04 16:05:22 +0530

    Add multiple formats to print date of birth

---
+++ Print dob in different ways.py
@@ -0,0 +1,8 @@
+day = 7
+month = 9
+year = 2007
+
+print("DOB:", day, "/", month, "/", year)
+print("DOB:", day, "-", month, "-", year)
+print("DOB:", year, "-", month, "-", day)
+print("DOB: 07 September 2007")
