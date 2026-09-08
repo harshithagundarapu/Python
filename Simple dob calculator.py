1c01dad5d32a93fbbcf9ba22efb92802753d144c
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-09-08 20:54:34 +0530

    Create Simple dob calculator.py

---
+++ Simple dob calculator.py
@@ -0,0 +1,9 @@
+day = 7
+month = 9
+year = 2007
+
+print("===== MY DETAILS =====")
+print("Date       :", day)
+print("Month      :", month)
+print("Year       :", year)
+print("Full DOB   : 07 September 2007")
