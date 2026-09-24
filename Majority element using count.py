e07637daf659a553efcc93c9cb0f7f7b621030c0
Harshitha  gundarapu <harshithagundapu123@gmail.com>
2026-09-24 19:22:29 +0530

    Create Majority element using count.py

---
+++ Majority element using count.py
@@ -0,0 +1,15 @@
+nums1 = [4, 3, 2, 7, 8, 2, 3, 1]
+nums2 = [1, 1]
+
+def find_missing(nums):
+    n = len(nums)
+    result = []
+
+    for i in range(1, n + 1):
+        if nums.count(i) == 0:
+            result.append(i)
+
+    return result
+
+print("Example 1:", find_missing(nums1))
+print("Example 2:", find_missing(nums2))
